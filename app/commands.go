c9d43478b417547860aa679dad2b4749d4878a22
amstein4920 <amstein4920@gmail.com>
2024-10-05 04:36:22 +0800

    CLI tests (cli-test-1728074182571)

---
+++ app/commands.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type SavedData struct {
+	value     string
+	expirary  *int64
+	savedTime time.Time
+}
+
+func CommandsSwitch(commands []string, savedDataMap map[string]SavedData) string {
+	switch strings.ToUpper(commands[0]) {
+	case "PING":
+		return "+PONG\r\n"
+	case "ECHO":
+		return fmt.Sprintf("$%v\r\n%v\r\n", len(commands[1]), commands[1])
+	case "SET":
+		return setCommand(commands, savedDataMap)
+	case "GET":
+		getCommand(commands, savedDataMap)
+	}
+	return "$-1\r\n"
+}
+
+func setCommand(commands []string, savedDataMap map[string]SavedData) string {
+	if len(commands) < 4 {
+		savedDataMap[commands[1]] = SavedData{
+			value: commands[2],
+		}
+		return "+OK\r\n"
+	}
+
+	if strings.ToUpper(commands[3]) == "PX" {
+		expiraryValue, err := strconv.ParseInt(commands[4], 10, 64)
+		if err != nil {
+			fmt.Println("Improper Expirary Value")
+			return "$-1\r\n"
+		}
+		savedDataMap[commands[1]] = SavedData{
+			value:     commands[2],
+			expirary:  &expiraryValue,
+			savedTime: time.Now(),
+		}
+	}
+	return "+OK\r\n"
+}
+
+func getCommand(commands []string, savedDataMap map[string]SavedData) string {
+	savedDataForKey := savedDataMap[commands[1]]
+	if savedDataForKey.value == "" {
+		return "$-1\r\n"
+	}
+	fmt.Println(savedDataForKey.expirary)
+	if savedDataForKey.expirary == nil {
+		fmt.Println("HUH")
+		return fmt.Sprintf("$%v\r\n%v\r\n", len(savedDataForKey.value), savedDataForKey.value)
+	}
+	if *savedDataForKey.expirary < time.Since(savedDataForKey.savedTime).Milliseconds() {
+		return fmt.Sprintf("$%v\r\n%v\r\n", len(savedDataForKey.value), savedDataForKey.value)
+	}
+	return "$-1\r\n"
+}
