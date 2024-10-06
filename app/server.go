90d0e0ca35c8b52d80ad0ca7b7d5b8ad549f8ece
amstein4920 <amstein4920@gmail.com>
2024-10-07 01:23:28 +0800

    CLI tests (cli-test-1728235408921)

--- app/server.go
+++ app/server.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"net"
 	"os"
 	"strconv"
 )
 
+var portFlag *int
+
+func init() {
+	portFlag = flag.Int("port", 6379, "port value")
+}
+
 func main() {
-	listener, err := net.Listen("tcp", "0.0.0.0:6379")
+	flag.Parse()
+	fmt.Printf("Port: %v", *portFlag)
+	address := fmt.Sprintf("0.0.0.0:%v", *portFlag)
+	listener, err := net.Listen("tcp", address)
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
