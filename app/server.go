59fe58c8a501bbc4f2bd3555e1ae9ab0c2868218
amstein4920 <amstein4920@gmail.com>
2024-10-07 23:48:32 +0800

    CLI tests (cli-test-1728316112687)

--- app/server.go
+++ app/server.go
@@ -20,6 +20,23 @@ func init() {
 func main() {
 	flag.Parse()
 
+	if *replicaFlag != "" {
+		port, err := strconv.Atoi(flag.Arg(0))
+		if err != nil {
+			fmt.Println("Invalid leader/master port provided")
+			os.Exit(1)
+		}
+
+		address := fmt.Sprintf("%s:%d", *replicaFlag, port)
+		leaderConn, err := net.Dial("tcp", address)
+		if err != nil {
+			fmt.Println("Failed to connected to leader/master server")
+			os.Exit(1)
+		}
+
+		fmt.Fprint(leaderConn, "*1\r\n$4\r\nPING\r\n")
+	}
+
 	address := fmt.Sprintf("0.0.0.0:%v", *portFlag)
 	listener, err := net.Listen("tcp", address)
 	if err != nil {
